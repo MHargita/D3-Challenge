f30a4363079aaf60f660bde349c0744406cdc429
Michelle Davis <67066019+MHargita@users.noreply.github.com>
2021-01-14 22:15:42 -0500

    Updated index.html and README

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -6,7 +6,7 @@ var svgHeight = 500;
 var margin = {
     top: 20,
     right: 40,
-    bottom: 80,
+    bottom: 60,
     left: 100
 };
 
@@ -15,26 +15,110 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create SVG wrapper, append an SVG group (container) that will hold our chart
-var svg = d3
-    .select("scatter")
+var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
 // Append an SVG group (container)
 
-var ScatterGroup = svg.append("g")
+var chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
-
-
-
-
-
-    
 
 // Retrieve data from CSV file and execute
 d3.csv("data.csv").then(function(censusData) {
     console.log(censusData);
-}
+    // 1. Parse Data/Cast as numbers
+    censusData.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.obesity = +data.obesity;
+    });
+
+    // 2. Create Scale Functions- x scale
+    var xLinearScale = d3.scaleLinear()
+    .domain([8,d3.max(censusData, d => d.poverty)])
+    .range([0, width]);
+
+    // y scale
+
+    var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(censusData, d => d.obesity) - 1, d3.max(censusData, d => d.obesity) +2])
+    .range([height, 0]);
+
+    // 3. Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // 4. Append Axes to the chart
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // 5. Create Circles
+    var circlesGroup = chartGroup.append("g").selectAll("circle")
+    .data(censusData)
+    .enter()
+    .append("circle")
+    .classed("circle", true)
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.obesity))
+    .attr("r", "10")
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
+
+    // State abbreviations
+
+    var stateGroup = chartGroup.append("g").selectAll("text")
+    .data(censusData)
+    .enter()
+    .append("text")
+    .classed("state-text", true)
+    .attr("x", d => xLinearScale(d.poverty))
+    // .attr("y"), d => yLinearScale(d.obesity))
+    .attr("transform", `translate(-10, 5)`)
+    .text(d => d.abbr)
+    .attr("fill", "white");
+
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    var toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(function(d) {
+        return (`${d.abbr}<br>Poverty: ${d.poverty}<br>Obesity: ${d.obesity}`);
+        });
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+      chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+      circlesGroup.on("mouseover", function(data) {
+        toolTip.show(data, this);
+    });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Obesity Percentages");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Poverty Percentages");
+
+
+
+    }).catch(function(error) {
+    console.log(error);
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # D3-Challenge
+
+Using the d3 library and the provided data from the U.S. Census Bureau, I created a scatter plot comparing 2 variables: poverty and obesity. Each state is represented with a circle and labeled with its abbreviation. I created my axes and labels displayed at the bottom and left of the chart. I used my local python server to run the visualization.
